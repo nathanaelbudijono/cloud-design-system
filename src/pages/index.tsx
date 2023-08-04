6a0bd1e03c6864ee9af7b41d5596557d874b6476
Nathanael <Nathan@MacBook-Pro.local>
2023-08-04 13:57:18 +0700

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,19 @@ import sandbox from "@/constant/sandbox-link";
 export default function Home() {
   const { cards } = sandbox;
   return (
-    <main className="flex items-center justify-center flex-col">
+    <main className="flex items-center justify-center flex-col text-center">
       <Seo
         title="Cloud Design System"
         description="Design system used on projects"
       />
-      <Typography variant="h1">Cloud Design System</Typography>
+      <Typography variant="h2">Cloud Design System</Typography>
       <Typography variant="p">
         Check out the{" "}
         <ArrowLink href="https://github.com/budinathan" target="_blank">
           repository
         </ArrowLink>
       </Typography>
-      <section className="mt-2 flex flex-warp justify-center gap-2">
+      <section className="mt-2 flex flex-wrap justify-center gap-2">
         {cards.map((item, index) => (
           <Framer key={index} delay={index * 0.2}>
             <ButtonLink variant="outline" size="sm" href={item.link}>
