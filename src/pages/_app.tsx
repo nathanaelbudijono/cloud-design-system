e09328fe4857a64d33dc39c03cd07a9a34b3f02b
Nathanael <Nathan@MacBook-Pro.local>
2023-08-05 18:35:22 +0700

    update layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <section className=" mx-auto px-16 py-10 bg-gradient">
+    <section className=" mx-auto px-16 py-10 bg-gradient max-md:px-6 max-md:py-3">
       <Component {...pageProps} />
     </section>
   );
