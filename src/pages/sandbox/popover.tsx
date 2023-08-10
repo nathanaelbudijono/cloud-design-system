5cf6d96ca8c3eb9291cee426399a4200c5999453
Nathanael <Nathan@MacBook-Pro.local>
2023-08-10 10:29:24 +0700

    Add popover

--- src/pages/sandbox/popover.tsx
+++ src/pages/sandbox/popover.tsx
@@ -1,9 +1,16 @@
+import Seo from "@/components/core/seo";
 import Typography from "@/components/core/typography";
+import ExamplePopover from "@/components/modules/popover/popover-example";
 
 export default function Popovers() {
   return (
     <main>
+      <Seo title="Popover" description="popover page" />
       <Typography variant="h4">Popover</Typography>
+      <section className="flex justify-center">
+        {" "}
+        <ExamplePopover />
+      </section>
     </main>
   );
 }
