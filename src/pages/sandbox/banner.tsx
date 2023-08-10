e14e97cadf70d3cde20b158f41392d0ad99e92c2
Nathanael <Nathan@MacBook-Pro.local>
2023-08-10 10:43:27 +0700

    update banner page

--- src/pages/sandbox/banner.tsx
+++ src/pages/sandbox/banner.tsx
@@ -1,14 +1,18 @@
 import Banner from "@/components/core/banner";
 import Seo from "@/components/core/seo";
 import Typography from "@/components/core/typography";
+import UnstyledLink from "@/components/links/unstyled-link";
 
 export default function Banners() {
   return (
     <main>
       <Seo title="Banner" description="banner" />
       <section className="fixed w-full left-0 text-center top-0 flex flex-col z-40">
-        <div className="py-2 bg-d-700 ">
-          <Typography variant="h1">Navigation Bar</Typography>
+        <div className="py-4 bg-d-700 flex justify-between px-10 items-center">
+          <Typography variant="large">Cloud DS</Typography>
+          <UnstyledLink href="/" className="text-color-100">
+            Home
+          </UnstyledLink>
         </div>
         <Banner />
       </section>
