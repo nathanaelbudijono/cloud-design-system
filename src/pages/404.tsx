f7e3172e337f5aa4805464b56071dbdc2c16e22f
Nathanael <Nathan@MacBook-Pro.local>
2023-08-11 13:58:57 +0700

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,13 @@
 import Seo from "@/components/core/seo";
 import Typography from "@/components/core/typography";
 import ButtonLink from "@/components/links/button-links";
+import Image from "next/image";
 
 export default function NotFoundPage() {
   return (
     <main className="flex justify-center items-center flex-col gap-4">
       <Seo title="Not Found" />
-      <img src="/404.png" alt="404" className="sm:max-w-[400px]" />
+      <Image src="/404.png" alt="404" className="sm:max-w-[400px]" />
       <Typography variant="h3" className="text-center">
         Oops, looks like you're lost
       </Typography>
