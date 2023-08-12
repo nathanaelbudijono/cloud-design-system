dd6eb7d61a2bce40b671885530f7943766a0eeb9
Nathanael <Nathan@MacBook-Pro.local>
2023-08-12 11:13:52 +0700

    added checkbox page

---
+++ src/pages/sandbox/checkbox.tsx
@@ -0,0 +1,11 @@
+import Seo from "@/components/core/seo";
+import Typography from "@/components/core/typography";
+
+export default function Checkboxs() {
+  return (
+    <main>
+      <Seo title="Checkbox" description="Checkbox" />
+      <Typography variant="h4">Checkbox</Typography>
+    </main>
+  );
+}
