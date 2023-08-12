246b2074dfbe6dfc6f7d93d7d02797acb1962282
Nathanael <Nathan@MacBook-Pro.local>
2023-08-12 11:13:09 +0700

    Add radio button page

---
+++ src/pages/sandbox/radio-button.tsx
@@ -0,0 +1,11 @@
+import Seo from "@/components/core/seo";
+import Typography from "@/components/core/typography";
+
+export default function RadioButtons() {
+  return (
+    <main>
+      <Seo title="Radio Button" description="Radio button" />
+      <Typography variant="h4">Radio Button</Typography>
+    </main>
+  );
+}
