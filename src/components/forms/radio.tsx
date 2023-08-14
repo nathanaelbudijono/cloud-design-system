c60b6b910433d057547b80ec578fde2d223126cd
Nathanael <Nathan@MacBook-Pro.local>
2023-08-14 09:42:23 +0700

    update radio css

--- src/components/forms/radio.tsx
+++ src/components/forms/radio.tsx
@@ -64,7 +64,7 @@ export default function Radio({
         </Typography>
 
         {!hideError && error && (
-          <Typography variant="small" color="danger" className="mt-1">
+          <Typography variant="small" color="danger" className="mt-2">
             {error?.message?.toString()}
           </Typography>
         )}
