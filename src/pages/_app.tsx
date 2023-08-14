6e282de81721327b3a7cc1e09838dd6a65dea701
Nathanael <Nathan@MacBook-Pro.local>
2023-08-14 12:54:45 +0700

    add nextjs-progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <section className="mx-auto px-16 py-10 bg-gradient max-md:px-6 max-md:py-3">
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        height={2}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </section>
   );
