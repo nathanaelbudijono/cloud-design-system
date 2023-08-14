de6f8eb2d0b02b057201c41d4d85b92dd25e8649
Nathanael <Nathan@MacBook-Pro.local>
2023-08-14 10:00:58 +0700

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import Seo from "@/components/core/seo";
+import Typography from "@/components/core/typography";
+import ButtonLink from "@/components/links/button-links";
+
+export default function NotFoundPage() {
+  return (
+    <main className="flex flex-col gap-2 items-center justify-center text-center">
+      <Seo title="Not Found" description="Not Found" />
+      <img src={"/404.png"} className="sm:max-w-lg" alt="Not found" />
+      <Typography variant="p">
+        Oops, looks like you wondered to the wrong place.
+      </Typography>
+      <ButtonLink href="/" variant="primary">
+        Return Home
+      </ButtonLink>
+    </main>
+  );
+}
