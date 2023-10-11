fa1919e014a8d8d405e2e22489e9a06b1c9c9f6c
Nathanael <Nathan@MacBook-Pro.local>
2023-10-11 20:07:08 +0700

    create sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+const siteUrl = "https://cloud-design-system.vercel.app";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
